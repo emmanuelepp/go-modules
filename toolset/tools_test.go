7448424e76e66411dce95273da23e160cbcc2179
Emma <emmanuelponciano@gmail.com>
2024-03-16 21:30:51 -0400

    fixing test

--- toolset/tools_test.go
+++ toolset/tools_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestTools_RandomStringDifferentLength(t *testing.T) {
 	var testTools Tools
-	s := testTools.RandomString(15)
+	s := testTools.RandomPassword(15)
 	if len(s) != 15 {
 		t.Errorf("Expected string of length 15, got %d", len(s))
 	}
@@ -12,8 +12,8 @@ func TestTools_RandomStringDifferentLength(t *testing.T) {
 
 func TestTools_RandomStringRandomness(t *testing.T) {
 	var testTools Tools
-	s1 := testTools.RandomString(10)
-	s2 := testTools.RandomString(10)
+	s1 := testTools.RandomPassword(10)
+	s2 := testTools.RandomPassword(10)
 	if s1 == s2 {
 		t.Error("RandomString returned the same string twice")
 	}
